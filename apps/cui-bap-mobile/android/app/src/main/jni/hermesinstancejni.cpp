9a4546380b7b80249b017bbb2185b92f0bf8f0b0
lbngu <lbngung19@gmail.com>
2026-05-04 23:45:15 +0700

    fix: add HermesInstance initHybrid stub

--- apps/cui-bap-mobile/android/app/src/main/jni/hermesinstancejni.cpp
+++ apps/cui-bap-mobile/android/app/src/main/jni/hermesinstancejni.cpp
@@ -1,2 +1,11 @@
 #include <jni.h>
-extern "C" {}
+
+extern "C" {
+
+JNIEXPORT jobject JNICALL
+Java_com_facebook_react_runtime_hermes_HermesInstance_initHybrid(
+    JNIEnv* env, jclass clazz, jobject jsRuntimeFactory, jboolean enableDebugger) {
+    return nullptr;
+}
+
+} // extern "C"
