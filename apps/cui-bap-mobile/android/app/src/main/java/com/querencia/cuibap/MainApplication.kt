c95bd7d7767337331b762f52bd1a27866e0b324c
lbngu <lbngung19@gmail.com>
2026-05-04 22:27:45 +0700

    fix: add reactHost for new architecture

--- apps/cui-bap-mobile/android/app/src/main/java/com/querencia/cuibap/MainApplication.kt
+++ apps/cui-bap-mobile/android/app/src/main/java/com/querencia/cuibap/MainApplication.kt
@@ -1,29 +1,33 @@
 package com.querencia.cuibap
 
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
+
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-            }
+        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = false
         override val isNewArchEnabled: Boolean = true
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
-override fun onCreate() {
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
     super.onCreate()
-    System.loadLibrary("reactnative")
     SoLoader.init(this, false)
     load()
-}}
+  }
+}
