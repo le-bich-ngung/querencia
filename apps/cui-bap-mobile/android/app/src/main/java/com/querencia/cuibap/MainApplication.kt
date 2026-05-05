02d142dff01a315b4281f7d54504c331369ff49a
lbngu <lbngung19@gmail.com>
2026-05-05 15:55:44 +0700

    fix: switch to old architecture

--- apps/cui-bap-mobile/android/app/src/main/java/com/querencia/cuibap/MainApplication.kt
+++ apps/cui-bap-mobile/android/app/src/main/java/com/querencia/cuibap/MainApplication.kt
@@ -3,11 +3,8 @@ package com.querencia.cuibap
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
@@ -18,16 +15,12 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> = PackageList(this).packages
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = false
-        override val isNewArchEnabled: Boolean = true
+        override val isNewArchEnabled: Boolean = false
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    load()
   }
 }
