962e638e62c48069c841129bb1564c02de08c3b5
lbngu <lbngung19@gmail.com>
2026-05-08 01:57:38 +0700

    fix: add all featureflags stubs at once

--- apps/cui-bap-mobile/android/app/src/main/jni/stub.cpp
+++ apps/cui-bap-mobile/android/app/src/main/jni/stub.cpp
@@ -1,55 +1,67 @@
 #include <jni.h>
 
+#define FF(name) \
+JNIEXPORT jboolean JNICALL \
+Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_##name(\
+    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
+
 extern "C" {
 
-// ReactNativeFeatureFlags stubs
 JNIEXPORT void JNICALL
 Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_override(
     JNIEnv* env, jclass clazz, jobject provider) {}
 
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_commonTestFlag(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
+JNIEXPORT void JNICALL
+Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_dangerouslyReset(
+    JNIEnv* env, jclass clazz) {}
 
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_fuseboxEnabledDebug(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
+FF(commonTestFlag)
+FF(allowRecursiveCommitsWithSynchronousMountOnAndroid)
+FF(batchRenderingUpdatesInEventLoop)
+FF(completeReactInstanceCreationOnBgThreadOnAndroid)
+FF(destroyFabricSurfacesInReactInstanceManager)
+FF(enableAlignItemsBaselineOnFabricIOS)
+FF(enableAndroidMixBlendModeProp)
+FF(enableBackgroundStyleApplicator)
+FF(enableCleanTextInputYogaNode)
+FF(enableEagerRootViewAttachment)
+FF(enableEventEmitterRetentionDuringGesturesOnAndroid)
+FF(enableFabricLogs)
+FF(enableFabricRendererExclusively)
+FF(enableGranularShadowTreeStateReconciliation)
+FF(enableIOSViewClipToPaddingBox)
+FF(enableLayoutAnimationsOnIOS)
+FF(enableLongTaskAPI)
+FF(enableMicrotasks)
+FF(enablePropsUpdateReconciliationAndroid)
+FF(enableReportEventPaintTime)
+FF(enableSynchronousStateUpdates)
+FF(enableUIConsistency)
+FF(enableViewRecycling)
+FF(excludeYogaFromRawProps)
+FF(fetchImagesInViewPreallocation)
+FF(fixIncorrectScrollViewStateUpdateOnAndroid)
+FF(fixMappingOfEventPrioritiesBetweenFabricAndReact)
+FF(fixMissedFabricStateUpdatesOnAndroid)
+FF(fixMountingCoordinatorReportedPendingTransactionsOnAndroid)
+FF(forceBatchingMountItemsOnAndroid)
+FF(fuseboxEnabledDebug)
+FF(fuseboxEnabledRelease)
+FF(initEagerTurboModulesOnNativeModulesQueueAndroid)
+FF(lazyAnimationCallbacks)
+FF(loadVectorDrawablesOnImages)
+FF(setAndroidLayoutDirection)
+FF(traceTurboModulePromiseRejectionsOnAndroid)
+FF(useFabricInterop)
+FF(useImmediateExecutorInAndroidBridgeless)
+FF(useModernRuntimeScheduler)
+FF(useNativeViewConfigsInBridgelessMode)
+FF(useNewReactImageViewBackgroundDrawing)
+FF(useOptimisedViewPreallocationOnAndroid)
+FF(useOptimizedEventBatchingOnAndroid)
+FF(useRuntimeShadowNodeReferenceUpdate)
+FF(useRuntimeShadowNodeReferenceUpdateOnLayout)
+FF(useStateAlignmentMechanism)
+FF(useTurboModuleInterop)
 
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_fuseboxEnabledRelease(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableFabricLogs(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableMicrotasks(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableViewRecycling(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_useFabricInterop(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-// HermesExecutor stub for Old Architecture
-JNIEXPORT jobject JNICALL
-Java_com_facebook_hermes_reactexecutor_HermesExecutor_initHybridDefaultConfig(
-    JNIEnv* env, jclass clazz, jboolean enableDebugger, jstring debuggerName) {
-    return nullptr;
-}
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableEagerRootViewAttachment(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableLongTaskAPI(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
-
-JNIEXPORT jboolean JNICALL
-Java_com_facebook_react_internal_featureflags_ReactNativeFeatureFlagsCxxInterop_enableUIConsistency(
-    JNIEnv* env, jclass clazz) { return JNI_FALSE; }
 } // extern "C"
